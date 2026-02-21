5ea0b5573ff56b75b2d61f6fadf5e1a2ba2e4863
rvlto974 <jacquet-m@protonmail.com>
2026-02-21 07:50:26 +0400

    feat: système de commandes fonctionnel

---
+++ vite-gourmand02/controllers/CommandesController.php
@@ -0,0 +1,71 @@
+<?php
+require_once 'models/CommandeModel.php';
+require_once 'models/MenuModel.php';
+
+class CommandesController {
+    private $commandeModel;
+    private $menuModel;
+
+    public function __construct() {
+        $this->commandeModel = new CommandeModel();
+        $this->menuModel = new MenuModel();
+    }
+
+    private function verifierConnexion() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (!isset($_SESSION['user_id'])) {
+            header('Location: /auth/connexion');
+            exit;
+        }
+    }
+
+    public function nouveau() {
+        $this->verifierConnexion();
+
+        $menu_id = $_GET['menu_id'] ?? null;
+        $nb_personnes = $_GET['nb_personnes'] ?? 1;
+
+        if (!$menu_id) {
+            header('Location: /menus');
+            exit;
+        }
+
+        $menu = $this->menuModel->getById($menu_id);
+
+        if (!$menu) {
+            header('Location: /menus');
+            exit;
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nb_personnes = $_POST['nb_personnes'] ?? $menu['nb_personnes_min'];
+            $adresse = $_POST['adresse_livraison'] ?? '';
+            $date = $_POST['date_prestation'] ?? '';
+
+            $prix_total = ($menu['prix_base'] / $menu['nb_personnes_min']) * $nb_personnes;
+
+            $commande_id = $this->commandeModel->creer([
+                'utilisateur_id' => $_SESSION['user_id'],
+                'menu_id' => $menu_id,
+                'nb_personnes' => $nb_personnes,
+                'prix_total' => $prix_total,
+                'adresse_livraison' => $adresse,
+                'date_prestation' => $date
+            ]);
+
+            header('Location: /commandes/confirmation?id=' . $commande_id);
+            exit;
+        }
+
+        require_once 'views/commandes/nouveau.php';
+    }
+
+    public function confirmation() {
+        $this->verifierConnexion();
+        $id = $_GET['id'] ?? null;
+        $commande = $this->commandeModel->getById($id);
+        require_once 'views/commandes/confirmation.php';
+    }
+}
